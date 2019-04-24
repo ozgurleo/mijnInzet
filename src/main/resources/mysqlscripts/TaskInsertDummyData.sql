c3730a12ee2e1295220297b371bd79c254d086c0
Brahim <b.ettoghri@gmail.com>
2019-04-24 15:53:18 +0200

    TaskInsertDymmyData.sql in resources folder toegevoegd

---
+++ src/main/resources/mysqlscripts/TaskInsertDummyData.sql
@@ -0,0 +1,19 @@
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (1,'java programming',32,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (2,'C++ programming',16,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (3,'AI analyse',8,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (4,'Stage begeleiding',24,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (5,'MIW project ondersteuning',200,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (6,'OOP advanced',32,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (7,'C++ problems and solutions',16,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (8,'Googlen voor studenten',16,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (9,'pascal',16,3);
+INSERT INTO Task (taskId,taskName,estimatedHours,yearsToExpiryDate) VALUES (10,'Fourier analyse module1',16,3);
+
+
+
+
+
+
+
+
+
