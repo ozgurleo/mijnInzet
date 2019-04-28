8b3d7299ebfb91f03d303d60f8682009b1c98d3d
E.Wit <esther.wit@hva.nl>
2019-04-28 10:58:36 +0200

    commit

--- src/main/java/com/mijninzet/projectteamdrie/controller/TimeTableController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/TimeTableController.java
@@ -1,4 +1,15 @@
 package com.mijninzet.projectteamdrie.controller;
 
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class TimeTableController {
+    SessionFactory sessionFactory;
+
+    @RequestMapping("timeTable")
+    public String hello() {
+        return "timeTable";
+    }
 }
