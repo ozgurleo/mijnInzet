8cb61365bfcde9391bb7547b5c842949a182fa00
Brahim <b.ettoghri@gmail.com>
2019-05-06 13:02:40 +0200

    commit

--- src/main/java/com/mijninzet/projectteamdrie/repository/TaskApplicationRepository.java
+++ src/main/java/com/mijninzet/projectteamdrie/repository/TaskApplicationRepository.java
@@ -2,6 +2,7 @@ package com.mijninzet.projectteamdrie.repository;
 
 
 import com.mijninzet.projectteamdrie.model.entity.TaskApplication;
+import com.mijninzet.projectteamdrie.model.entity.TaskApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskApplicationRepository extends JpaRepository<TaskApplication, Integer> {
