0976ee1a25a3fc8b4b1e111a81d2aec12033d1da
ozgurleo <alpozguraslan@gmail.com>
2019-05-06 15:49:04 +0200

    post new schedule

--- src/main/java/com/mijninzet/projectteamdrie/controller/StaffAvailibilityController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/StaffAvailibilityController.java
@@ -1,33 +1,29 @@
-//package com.mijninzet.projectteamdrie.controller;
-//
-//import com.mijninzet.projectteamdrie.model.entity.StaffAvailability;
-//import com.mijninzet.projectteamdrie.model.entity.TimeTable;
-//import com.mijninzet.projectteamdrie.model.entity.user.Teacher;
-//import com.mijninzet.projectteamdrie.model.entity.user.User;
-//import com.mijninzet.projectteamdrie.service.StaffAvailibilityService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//public class StaffAvailibilityController {
-//    @Autowired
-//    private StaffAvailibilityService staffAvailibilityService;
-//
-//    @RequestMapping("hello/schedule/{id}")
-//    public List<StaffAvailability> getAllSchedule(@PathVariable Integer id){
-//        return staffAvailibilityService.getAllStaffAvailibility(id);
-//    }
-//
-//    @PostMapping("hello/schedule/{userId}/{timeTableId}/newschedule")
-//    public void addStaffAvailiblity(@RequestBody StaffAvailability sa, @PathVariable Integer userId,@PathVariable Integer timeTableId){
-//        sa.setUser(new Teacher(userId));
-//        sa.setTimeTable(new TimeTable(timeTableId,0,0,0));
-//
-//        staffAvailibilityService.addStaffAvailibility(sa);
-//
-//
-//    }
-//}
-//
+package com.mijninzet.projectteamdrie.controller;
+
+import com.mijninzet.projectteamdrie.model.entity.StaffAvailability;
+import com.mijninzet.projectteamdrie.model.entity.user.Teacher;
+import com.mijninzet.projectteamdrie.service.StaffAvailibilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class StaffAvailibilityController {
+    @Autowired
+    private StaffAvailibilityService staffAvailibilityService;
+
+    @RequestMapping("hello/schedule/{id}")
+    public List<StaffAvailability> getAllSchedule(@PathVariable Integer id){
+        return staffAvailibilityService.getAllStaffAvailibility(id);
+    }
+
+    @PostMapping("hello/schedule/{userId}/new")
+    public void addStaffAvailiblity(@RequestBody StaffAvailability sa, @PathVariable int userId){
+        sa.setUser(new Teacher(userId));
+        staffAvailibilityService.addStaffAvailibility(sa);
+
+
+    }
+}
+
--- src/main/java/com/mijninzet/projectteamdrie/repository/StaffAvailibilityRepository.java
+++ src/main/java/com/mijninzet/projectteamdrie/repository/StaffAvailibilityRepository.java
@@ -1,10 +1,10 @@
-//package com.mijninzet.projectteamdrie.repository;
-//
-//
-//import com.mijninzet.projectteamdrie.model.entity.StaffAvailability;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface StaffAvailibilityRepository extends JpaRepository<StaffAvailability,Integer> {
-//}
+package com.mijninzet.projectteamdrie.repository;
+
+
+import com.mijninzet.projectteamdrie.model.entity.StaffAvailability;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffAvailibilityRepository extends JpaRepository<StaffAvailability,Integer> {
+}
