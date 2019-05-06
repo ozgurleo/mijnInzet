64ecfd13385d575125532bb858d02c82d3326183
E.Wit <esther.wit@hva.nl>
2019-05-06 21:51:11 +0200

    Merge branch 'master' of https://gitlab.fdmci.hva.nl/zuiderh/nopressure
    # Conflicts:
    #	src/main/java/com/mijninzet/projectteamdrie/controller/TimeTableController.java
    #	src/main/java/com/mijninzet/projectteamdrie/model/entity/TimeTable.java
    #	src/main/java/com/mijninzet/projectteamdrie/repository/TimeTableRepository.java

--- src/main/java/com/mijninzet/projectteamdrie/controller/StaffAvailibilityController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/StaffAvailibilityController.java
@@ -2,28 +2,34 @@ package com.mijninzet.projectteamdrie.controller;
 
 import com.mijninzet.projectteamdrie.model.entity.StaffAvailability;
 import com.mijninzet.projectteamdrie.model.entity.user.Teacher;
+import com.mijninzet.projectteamdrie.repository.StaffAvailibilityRepository;
 import com.mijninzet.projectteamdrie.service.StaffAvailibilityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Controller
 public class StaffAvailibilityController {
     @Autowired
     private StaffAvailibilityService staffAvailibilityService;
+    @Autowired
+    StaffAvailibilityRepository availibilityRepository;
 
-    @RequestMapping("hello/schedule/{id}")
-    public List<StaffAvailability> getAllSchedule(@PathVariable Integer id){
-        return staffAvailibilityService.getAllStaffAvailibility(id);
-    }
 
-    @RequestMapping(value="hello/schedule")
+    @RequestMapping(value="/schedule")
     public String showSchedule (){
         return "schedule";
     }
 
-    @PostMapping("hello/schedule/{userId}/new")
+    @RequestMapping("schedule/{id}")
+    public List<StaffAvailability> getAllSchedule(@PathVariable Integer id){
+        return staffAvailibilityService.getAllStaffAvailibility(id);
+    }
+
+    @PostMapping("schedule/{userId}/new")
     public void addStaffAvailiblity(@RequestBody StaffAvailability sa, @PathVariable int userId){
         sa.setUser(new Teacher(userId));
         staffAvailibilityService.addStaffAvailibility(sa);
