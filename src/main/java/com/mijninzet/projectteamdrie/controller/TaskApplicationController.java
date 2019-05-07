157047d651185f2cbc329ed787e9ca8ba31c5835
Brahim <b.ettoghri@gmail.com>
2019-05-07 19:39:39 +0200

    nieuwe sql script toegevoegd in resources/mysqlscripts map

--- src/main/java/com/mijninzet/projectteamdrie/controller/TaskApplicationController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/TaskApplicationController.java
@@ -3,16 +3,18 @@ package com.mijninzet.projectteamdrie.controller;
 import com.mijninzet.projectteamdrie.model.entity.TaskApplication;
 import com.mijninzet.projectteamdrie.repository.TaskApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskApplicationController {
 
     @Autowired
     TaskApplicationRepository taskApplicationRepo;
 
-    @RequestMapping(value = "/showTasks")
-    public void storeTaskApplication(TaskApplication taskAppl) {
-
-    }
+      
 
 }
--- src/main/java/com/mijninzet/projectteamdrie/controller/TaskController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TaskController {
@@ -23,6 +24,7 @@ public class TaskController {
         model.addAttribute("showTasks", taskRepository.getVacancies());
         return "showTasks";
     }
+
 
 
 }
