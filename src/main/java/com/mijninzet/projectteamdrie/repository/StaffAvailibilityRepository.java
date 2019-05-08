4b10ba33311f0f20eded160adb393165a86bbe1f
Brahim <b.ettoghri@gmail.com>
2019-05-08 10:44:04 +0200

    Added 2 methods to class TaskApplicationController

--- src/main/java/com/mijninzet/projectteamdrie/repository/StaffAvailibilityRepository.java
+++ src/main/java/com/mijninzet/projectteamdrie/repository/StaffAvailibilityRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface StaffAvailibilityRepository extends JpaRepository<StaffAvailability,Integer> {
 
+    List<StaffAvailability> findByUserId(int id);
+
 }
