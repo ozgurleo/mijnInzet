ef77f83ff71e60ecf305be446c0dfdd3035a7b4f
Brahim <b.ettoghri@gmail.com>
2019-05-12 12:44:00 +0200

    minor change

--- src/main/resources/templates/brahimTask.js
+++ src/main/resources/templates/brahimTask.js
@@ -1,12 +1,13 @@
 
 /*
-hier wordt de table mee opgeslagen in de local storage met key=showTaskTable en
+hier wordt de table mee opgeslagen in de sessionstorage met key=showTaskTable en
 met value = de tabel met id="taskTable"in de showTasks.html
  */
-localStorage.setItem( "showTaskTable", $("#taskTable").html() );
+sessionStorage.setItem( "showTaskTable", $("#taskTable").html() );
 
 /*
-hier wordt de table opgehaald uit de localStorage met key=showTaskTable
+hier wordt de table opgehaald uit de sessionlStorage met key=showTaskTable en in
+in de variable "applicationBasketTableData" gestopt
  */
-var applicationBasketTableData  = localStorage.getItem( "showTaskTable" );
+var applicationBasketTableData  = sessionStorage.getItem( "showTaskTable" );
 $("#taskTable").html( showTaskTable );
