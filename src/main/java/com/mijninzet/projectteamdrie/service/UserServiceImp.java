781794b3a8091a24575b6c041d0d03711c4e0da0
Semih217706 <semihkasimoglu@gmail.com>
2019-05-14 14:05:33 +0200

    ..

--- src/main/java/com/mijninzet/projectteamdrie/service/UserServiceImp.java
+++ src/main/java/com/mijninzet/projectteamdrie/service/UserServiceImp.java
@@ -1,14 +1,15 @@
 package com.mijninzet.projectteamdrie.service;
+
 import java.util.*;
 
 import com.mijninzet.projectteamdrie.model.entity.user.Role;
+import com.mijninzet.projectteamdrie.model.entity.user.RoleEnum;
 import com.mijninzet.projectteamdrie.model.entity.user.User;
 import com.mijninzet.projectteamdrie.repository.RoleRepository;
 import com.mijninzet.projectteamdrie.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 
 @Service
@@ -25,10 +26,40 @@ public class UserServiceImp implements UserService {
     public void saveUser(User user) {
         user.setPassword(encoder.encode(user.getPassword()));
         user.setStatus("VERIFIED");
+<<<<<<< HEAD
         Role userRole = roleRepository.findByRole("TEACHER");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+=======
+        user.setRoles(new HashSet<Role>(Arrays.asList(selectRole())));
+>>>>>>> ed77a066698c4273439763abdb5328a667684135
         userRepository.save(user);
+    }
 
+    private Role selectRole() {
+        // needs to be connected to frondend with dropdown menu with available roles
+        String roleDescriptor = "TEACHER";
+        Role userRole;
+        switch (roleDescriptor) {
+            case "TEACHER":
+                userRole = roleRepository.findByRole(RoleEnum.TEACHER.getRoleDescriptor());
+                break;
+            case "ADMIN":
+                userRole = roleRepository.findByRole(RoleEnum.ADMINISTRATOR.getRoleDescriptor());
+                break;
+            case "MANAGER":
+                userRole = roleRepository.findByRole(RoleEnum.MANAGER.getRoleDescriptor());
+                break;
+            case "SCHEDULER":
+                userRole = roleRepository.findByRole(RoleEnum.SCHEDULER.getRoleDescriptor());
+                break;
+            case "COORDINATOR":
+                userRole = roleRepository.findByRole(RoleEnum.COORDINATOR.getRoleDescriptor());
+                break;
+            default:
+                userRole = roleRepository.findByRole(RoleEnum.DEFAULT.getRoleDescriptor());
+                break;
+        }
+        return userRole;
     }
 
     public User findByEmail(String email){
@@ -46,30 +77,34 @@ public class UserServiceImp implements UserService {
         }
         return isUserAlreadyExists;
     }
+<<<<<<< HEAD
 
 
     public List<User> getAllUsers(){
+=======
+>>>>>>> ed77a066698c4273439763abdb5328a667684135
 
-        List<User> users=new ArrayList<>();
+    public List<User> getAllUsers() {
+
+        List<User> users = new ArrayList<>();
         userRepository.findAll().forEach(users::add);
         return users;
 
     }
-    public Optional<User> getUser(int id){
-       return userRepository.findById(id);
+
+    public Optional<User> getUser(int id) {
+        return userRepository.findById(id);
     }
 
-    public void addUser(User user){
+    public void addUser(User user) {
         userRepository.save(user);
     }
 
-    public void updateUser(int id,User user){
+    public void updateUser(int id, User user) {
         userRepository.save(user);
     }
 
-    public void deleteUser(int id){
+    public void deleteUser(int id) {
         userRepository.deleteById(id);
     }
-
-
 }
--- src/main/resources/mysqlscripts/InsertDummyData.sql
+++ src/main/resources/mysqlscripts/InsertDummyData.sql
@@ -6,11 +6,11 @@ INSERT INTO `mijn_inzet`.`subject` (`subject_id`, `estimated_hours`, `subject_na
 INSERT INTO `mijn_inzet`.`subject` (`subject_id`, `estimated_hours`, `subject_name`, `years_to_expiry_date`) VALUES ('2', '30', 'OOP', '2');
 INSERT INTO `mijn_inzet`.`subject` (`subject_id`, `estimated_hours`, `subject_name`, `years_to_expiry_date`) VALUES ('3', '20', 'Databases', '2');
 
-INSERT INTO role VALUES (1,'ADMIN_USER','This user has admin rights for administrative work');
-INSERT INTO role VALUES (2,'TEACHER','teacher');
-INSERT INTO role VALUES (3,'COORDINATOR','coordinator');
-INSERT INTO role VALUES (4,'SCHEDULER','This user has admin rights for administrative work');
-INSERT INTO role VALUES (5,'MANAGER','This user has admin rights for administrative work');
+INSERT INTO role (role_id, role_name, role_desc) VALUES (1,'ADMIN','This user has admin rights for administrative work');
+INSERT INTO role (role_id, role_name, role_desc) VALUES (2,'TEACHER','teacher');
+INSERT INTO role (role_id, role_name, role_desc) VALUES (3,'COORDINATOR','coordinator');
+INSERT INTO role (role_id, role_name, role_desc) VALUES (4,'SCHEDULER','This user has admin rights for administrative work');
+INSERT INTO role (role_id, role_name, role_desc) VALUES (5,'MANAGER','This user has admin rights for administrative work');
 
 insert into user (user_id,first_name,last_name,email,password,status) values (1,'hans','zuidervaart','admin@gmail.com','$2a$10$DD/FQ0hTIprg3fGarZl1reK1f7tzgM4RuFKjAKyun0Si60w6g3v5i','VERIFIED');
 insert into user_role (user_id, role_id) values ('1','1');
