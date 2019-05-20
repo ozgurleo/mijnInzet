0d71cf7fce79096dd906872989a70c6894ba39f4
Semih217706 <semihkasimoglu@gmail.com>
2019-05-20 15:50:09 +0200

    cascade is changed in users class

--- src/main/java/com/mijninzet/projectteamdrie/model/entity/user/User.java
+++ src/main/java/com/mijninzet/projectteamdrie/model/entity/user/User.java
@@ -50,7 +50,7 @@ public class User {
 
 
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade={CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
