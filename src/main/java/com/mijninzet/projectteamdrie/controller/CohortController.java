411486019c1829fb6900b1412ee20eb0be3e6e9f
Brahim <b.ettoghri@gmail.com>
2019-05-24 21:43:57 +0200

    getNumberOfCohortWeeks method toegeovegd in de CohortCOntroller om aantal weken inn cohort te bepalen

--- src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
@@ -1,5 +1,6 @@
 package com.mijninzet.projectteamdrie.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,19 +13,27 @@ import java.util.Date;
 @RequestMapping(value="/cohort")
 
 public class CohortController {
-
+public static final int DAYS_IN_WEEK=7;
 
     @RequestMapping(value="/createCohort")
     public String createCohort() {
         return "createCohort";
     }
 
+    public int getNumberOfCohortWeeks(LocalDate beginDate, LocalDate endDate){
+        int cohortWeeks=0;
+        //get nr off days in a cohort
+        long days = ChronoUnit.DAYS.between(beginDate, endDate);
 
-    public long getNumberOfCohortWeeks(LocalDate beginDate, LocalDate endDate){
+        // convert nr of days into whole weeks (ROUND UP)
+        if(days%DAYS_IN_WEEK>0){
+            cohortWeeks=(int) (days/DAYS_IN_WEEK) + 1;
+        }else{
+            cohortWeeks=(int) (days/DAYS_IN_WEEK);        }
 
-        long weeksInYear = ChronoUnit.WEEKS.between(beginDate, endDate);
-
-        return weeksInYear;
+        System.out.println(" getNumberOfCohortWeeks method is aangeroepen in de taskController");
+        System.out.println("Aantal DAGEN : " + days + "---> zijn in totaal " + cohortWeeks + " cohortWEKEN");
+        return cohortWeeks;
     }
 
 
