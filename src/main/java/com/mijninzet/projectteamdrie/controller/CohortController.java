b5efe7225cb6c89b2e82e2a7d82a8e77eea2a3af
E.Wit <esther.wit@hva.nl>
2019-05-25 11:12:41 +0200

    Merge branch 'EstherBranch'
    # Conflicts:
    #	src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
    #	src/main/java/com/mijninzet/projectteamdrie/model/entity/Cohort.java

--- src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
@@ -3,23 +3,17 @@ package com.mijninzet.projectteamdrie.controller;
 import com.mijninzet.projectteamdrie.model.entity.Cohort;
 import com.mijninzet.projectteamdrie.repository.CohortRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import com.mijninzet.projectteamdrie.model.entity.Cohort;
-import com.mijninzet.projectteamdrie.repository.CohortRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.sql.Date;
 import java.util.List;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 
@@ -31,7 +25,8 @@ public class CohortController {
 
     @Autowired
     CohortRepository cohortRepository;
-public static final int DAYS_IN_WEEK=7;
+
+    public static final int DAYS_IN_WEEK=7;
 
     @RequestMapping(value="/createCohort", method = RequestMethod.GET)
     public String getAllCohorts(Model model, Cohort cohort){
