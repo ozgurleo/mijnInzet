1011d53e5622fa2651e562e809e4bba8e5ee5dbf
Brahim <b.ettoghri@gmail.com>
2019-05-28 03:03:08 +0200

    minor

--- src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
+++ src/main/java/com/mijninzet/projectteamdrie/controller/CohortController.java
@@ -44,29 +44,7 @@ public class CohortController {
         return "createCohort";
     }
 
-    //hier worden het aantal weken in een cohort bepaalt
-    public int getNumberOfCohortWeeks(LocalDate beginDate, LocalDate endDate){
-        int cohortWeeks=0;
-        //get nr off days in a cohort
-        long days = ChronoUnit.DAYS.between(beginDate, endDate);
 
-        // convert nr of days into whole weeks (ROUND UP)
-        if(days%DAYS_IN_WEEK>0){
-            cohortWeeks=(int) (days/DAYS_IN_WEEK) + 1;
-        }else{
-            cohortWeeks=(int) (days/DAYS_IN_WEEK);        }
-
-        System.out.println(" getNumberOfCohortWeeks method is aangeroepen in de taskController");
-        System.out.println("Aantal DAGEN : " + days + "---> zijn in totaal " + cohortWeeks + " cohortWEKEN");
-        return cohortWeeks;
-    }
-
-
-    @GetMapping(value="/generateCohortSchedule")
-    public String generateCohortSchedule(){
-
-    return "generateCohortSchedule";
-    }
 
 
 }
---
+++ src/main/java/com/mijninzet/projectteamdrie/repository/CohortScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.mijninzet.projectteamdrie.repository;
+
+import com.mijninzet.projectteamdrie.model.entity.CohortSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CohortScheduleRepository extends JpaRepository<CohortSchedule, Integer> {
+
+
+}
